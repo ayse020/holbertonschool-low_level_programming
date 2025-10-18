657ba290bef0df41edb777c6ca39116990f05265
Ayse <ayse@example.com>
2025-10-18 19:07:14 +0000

    Fix Betty style issues in 4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char letter;
-    
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        if (letter != 'e' && letter != 'q')
-        {
-            putchar(letter);
-        }
-    }
-    
-    putchar('\n');
-    
-    return (0);
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
 }
