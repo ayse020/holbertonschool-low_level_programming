ff15b0247ad33ec703fddc67d6bb4ad30985b21e
Ayse <ayse@example.com>
2025-10-18 08:45:33 +0000

    Task 1: The last digit - Betty compliant

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,28 +9,25 @@
  */
 int main(void)
 {
-    int n;
-    int last_digit;
+	int n;
+	int last;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    
-    last_digit = n % 10;
-    
-    printf("Last digit of %d is %d and is ", n, last_digit);
-    
-    if (last_digit > 5)
-    {
-        printf("greater than 5\n");
-    }
-    else if (last_digit == 0)
-    {
-        printf("0\n");
-    }
-    else
-    {
-        printf("less than 6 and not 0\n");
-    }
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
 
-    return (0);
+	if (last > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	}
+	else if (last == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	}
+
+	return (0);
 }
