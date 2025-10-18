f6dc9b30dbf461974d6ba350a36230a662bab18f
Ayse <ayse@example.com>
2025-10-18 08:53:05 +0000

    Task 2: Print alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,15 +7,13 @@
  */
 int main(void)
 {
-    char letter = 'a';
-    
-    while (letter <= 'z')
-    {
-        putchar(letter);
-        letter++;
-    }
-    
-    putchar('\n');
-    
-    return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
 }
