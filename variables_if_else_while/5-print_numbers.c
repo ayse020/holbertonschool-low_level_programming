90014b485df8eedbf05b4f712018cd770d136174
Ayse <ayse@example.com>
2025-10-18 19:10:53 +0000

    Fix Betty style issues in 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int number;
-    
-    for (number = 0; number < 10; number++)
-    {
-        printf("%d", number);
-    }
-    
-    printf("\n");
-    
-    return (0);
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0);
 }
