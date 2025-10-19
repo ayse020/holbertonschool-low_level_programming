f792b9e4d09caa32ecd520473d4cbe0245628d54
Ayse <ayse@example.com>
2025-10-19 11:34:43 +0000

    Add 101-print_comb4.c - three digit combinations

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 
+/**
+ * main - Prints all possible combinations of three digits
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int i, j, k;
-    
-    for (i = 0; i < 8; i++)
-    {
-        for (j = i + 1; j < 9; j++)
-        {
-            for (k = j + 1; k < 10; k++)
-            {
-                putchar(i + '0');
-                putchar(j + '0');
-                putchar(k + '0');
-                
-                if (i != 7 || j != 8 || k != 9)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    
-    return (0);
+	int first;
+	int second;
+	int third;
+
+	for (first = 0; first <= 7; first++)
+	{
+		for (second = first + 1; second <= 8; second++)
+		{
+			for (third = second + 1; third <= 9; third++)
+			{
+				putchar(first + '0');
+				putchar(second + '0');
+				putchar(third + '0');
+
+				if (!(first == 7 && second == 8 && third == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
