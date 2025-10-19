d131e55df9ac0837a81bed07c298302edca66d7d
Ayse <ayse@example.com>
2025-10-19 11:28:44 +0000

    Add 100-print_comb3.c - two digit combinations

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int first;
+	int second;
+
+	for (first = 0; first <= 8; first++)
+	{
+		for (second = first + 1; second <= 9; second++)
+		{
+			putchar(first + '0');
+			putchar(second + '0');
+
+			if (!(first == 8 && second == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
