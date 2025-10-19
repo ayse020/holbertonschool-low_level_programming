34b2c6ad8d82e553ac5c546670fbdd866662d9f7
Ayse <ayse@example.com>
2025-10-19 11:06:57 +0000

    Fix Betty style issues

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int number;
-    
-    for (number = 0; number < 10; number++)
-    {
-        putchar(number + '0');
-    }
-    
-    putchar('\n');
-    
-    return (0);
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
+
+	return (0);
 }
