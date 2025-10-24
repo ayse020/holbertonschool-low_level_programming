506d9c7692e70fb3ca807b2f27c556b6948467cb
Ayse <ayse@example.com>
2025-10-24 09:43:10 +0000

    Fix 2-print_alphabet_x10: remove main.h dependency

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
-#include "main.h"
+#include <unistd.h>
 
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ */
 void print_alphabet_x10(void)
 {
     char letter;
     int i;
-    
-    /* Xarici döngü - 10 dəfə təkrar */
+
     for (i = 0; i < 10; i++)
     {
-        /* Daxili döngü - a-dan z-yə qədər hərflər */
         for (letter = 'a'; letter <= 'z'; letter++)
         {
-            _putchar(letter);  /* 1-ci _putchar istifadəsi */
+            write(1, &letter, 1);
         }
-        _putchar('\n');  /* 2-ci _putchar istifadəsi */
+        write(1, "\n", 1);
     }
 }
