be2ed0313ebd760f6f121be07e896ab2770408de
Ayse <ayse@example.com>
2025-10-30 18:21:16 +0000

    Fix Betty style: use tabs instead of spaces, proper indentation

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -4,26 +4,24 @@
  * print_rev - Prints a string in reverse followed by a new line
  * @s: The string to be printed in reverse
  *
- * Description: This function takes a string and prints it in reverse order
- * without modifying the original string.
+ * Return: void
  */
 void print_rev(char *s)
 {
-    int length = 0;
-    int i;
+	int length = 0;
+	int i;
 
-    /* String-in uzunluğunu tap - Find the length of the string */
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	/* Find the length of the string */
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
-    /* Son xarakterdən başlayaraq çap et - Print from last character to first */
-    for (i = length - 1; i >= 0; i--)
-    {
-        _putchar(s[i]);
-    }
+	/* Print from last character to first */
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
 
-    /* Yeni sətir əlavə et - Add new line */
-    _putchar('\n');
+	_putchar('\n');
 }
