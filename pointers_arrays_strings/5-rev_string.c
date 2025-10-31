d6cd0199a7bf4040923b7b5da72f9219669c1ece
Ayse <ayse@example.com>
2025-10-31 13:08:42 +0000

    style: Fix Betty coding style compliance

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,22 +6,29 @@
  */
 void rev_string(char *s)
 {
-    int length = 0;
-    int i = 0;
-    char temp;
-    
-    /* Find the length of the string */
-    while (s[length] != '\0')
-    {
-        length++;
-    }
-    
-    /* Swap characters from start and end moving towards center */
-    while (i < length / 2)
-    {
-        temp = s[i];
-        s[i] = s[length - i - 1];
-        s[length - i - 1] = temp;
-        i++;
-    }
+	int length = 0;
+	int start = 0;
+	int end;
+	char temp;
+
+	/* Find the length of the string */
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	end = length - 1; /* Position of last character */
+
+	/* Swap characters from start and end moving towards center */
+	while (start < end)
+	{
+		/* Swap characters */
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+
+		/* Move pointers towards center */
+		start++;
+		end--;
+	}
 }
