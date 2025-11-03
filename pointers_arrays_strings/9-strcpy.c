f06433f1cea0ab42bf4d2e786ae7c430e693480c
Ayse <ayse@example.com>
2025-11-03 09:00:37 +0000

    Fix Betty coding style - use tabs for indentation

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,26 +1,21 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies a string from src to dest including the null byte
- * @dest: Destination buffer where the string will be copied to
- * @src: Source string to be copied
+ * _strcpy - Copies a string from src to dest
+ * @dest: Destination buffer
+ * @src: Source string
  *
  * Return: Pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i = 0;
+	int i = 0;
 
-    /* Copy each character from src to dest until null terminator */
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-
-    /* Add the terminating null byte */
-    dest[i] = '\0';
-
-    /* Return pointer to dest */
-    return (dest);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
