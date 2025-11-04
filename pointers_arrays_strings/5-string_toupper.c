6128d1f753edcc4300573dd705cca3f45e8f4d43
Ayse <ayse@example.com>
2025-11-04 17:13:33 +0000

    Add string_toupper function

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: string to convert to uppercase
+ *
+ * Return: pointer to the converted string
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - 32;
+		}
+		ptr++;
+	}
+	return (str);
+}
