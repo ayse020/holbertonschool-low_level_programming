3884ca247190316d6d9b6773866326b0b03124b4
Ayse <ayse@example.com>
2025-11-13 19:05:52 +0000

    Add 0-whatsmyname program

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - prints program name
- * @argc: argument count
- * @argv: argument vector
- * Return: Always 0
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    printf("%s\n", argv[0]);
-    return (0);
+	(void)argc; /* Unused parameter */
+	printf("%s\n", argv[0]);
+	return (0);
 }
---
+++ argc_argv/README.md
@@ -0,0 +1,8 @@
+# 0-whatsmyname
+
+This program prints the name it was called with.
+
+## Usage
+
+```bash
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-whatsmyname.c -o 0-whatsmyname
