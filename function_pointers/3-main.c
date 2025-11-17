3ca3f4bc19258d648f5208d769bb8ea636cf1783
Ayse <ayse@example.com>
2025-11-17 18:03:08 +0000

    Fix Betty style: use tabs instead of spaces for indentation

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,40 +11,33 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*op_func)(int, int);
+	int num1, num2, result;
+	int (*op_func)(int, int);
 
-    /* Check if number of arguments is correct */
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    /* Convert arguments to integers */
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
 
-    /* Get the appropriate function for the operator */
-    op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    /* Check if operator is valid */
-    if (op_func == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    /* Check for division by zero */
-    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	result = op_func(num1, num2);
+	printf("%d\n", result);
 
-    /* Perform the operation and print result */
-    result = op_func(num1, num2);
-    printf("%d\n", result);
-
-    return (0);
+	return (0);
 }
