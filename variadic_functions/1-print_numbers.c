fcbb6bc091386bd5983884605aa88a6735a9adb8
Ayse <ayse@example.com>
2025-11-18 16:16:37 +0000

    Add print_numbers function that prints numbers with separators

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,37 +1,29 @@
-#include <stdarg.h>
+#include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_numbers - Prints numbers separated by a given string
- * @separator: The string to print between numbers
- * @n: The number of integers passed to the function
- * @...: Variable number of integers to print
+ * print_numbers - prints numbers with a separator
+ * @separator: string to print between numbers
+ * @n: number of integers passed to the function
  *
- * Description: If separator is NULL, it doesn't print it.
- *              Always prints a new line at the end.
+ * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
     va_list args;
     unsigned int i;
 
-    /* Initialize variable arguments */
     va_start(args, n);
 
-    /* Loop through all numbers */
     for (i = 0; i < n; i++)
     {
-        /* Print the current number */
         printf("%d", va_arg(args, int));
-
-        /* Print separator if not NULL and not last number */
+        
         if (separator != NULL && i < n - 1)
             printf("%s", separator);
     }
 
-    /* Clean up variable arguments */
     va_end(args);
-
-    /* Print new line at the end */
     printf("\n");
 }
