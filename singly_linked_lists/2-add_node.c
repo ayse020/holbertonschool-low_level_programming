a4291654adf7a72e7ed60d9e7d4fee40df307a29
Ayse <ayse@example.com>
2025-11-20 09:18:20 +0000

    Fix Betty coding style: function now under 40 lines

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -6,53 +6,38 @@
  * add_node - Adds a new node at the beginning of a list_t list
  * @head: Double pointer to the head of the list
  * @str: String to be duplicated and stored in the new node
- * 
+ *
  * Return: Address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    char *str_copy;
-    unsigned int len = 0;
+	list_t *new_node;
 
-    /* Check if head pointer is valid */
-    if (head == NULL)
-        return (NULL);
+	if (!head)
+		return (NULL);
 
-    /* Allocate memory for new node */
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
 
-    /* Duplicate the string */
-    if (str == NULL)
-    {
-        str_copy = NULL;
-    }
-    else
-    {
-        str_copy = strdup(str);
-        if (str_copy == NULL)
-        {
-            free(new_node);
-            return (NULL);
-        }
-    }
+	if (str)
+	{
+		new_node->str = strdup(str);
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->len = strlen(str);
+	}
+	else
+	{
+		new_node->str = NULL;
+		new_node->len = 0;
+	}
 
-    /* Calculate string length */
-    if (str != NULL)
-    {
-        while (str[len])
-            len++;
-    }
+	new_node->next = *head;
+	*head = new_node;
 
-    /* Initialize new node */
-    new_node->str = str_copy;
-    new_node->len = len;
-    new_node->next = *head;
-
-    /* Update head to point to new node */
-    *head = new_node;
-
-    return (new_node);
+	return (new_node);
 }
