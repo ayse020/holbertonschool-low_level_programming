6a39922856608ee86fdabed61dae0e2f6aadcbb8
Ayse <ayse@example.com>
2025-11-20 15:22:27 +0000

    Fix Betty style: reduce function length under 40 lines

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,48 +4,37 @@
 
 /**
  * add_node_end - Adds a new node at the end of a list_t list
- * @head: Double pointer to the head of the list
+ * @head: Pointer to the head of the list
  * @str: String to be duplicated and stored in the new node
  *
  * Return: Address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	list_t *temp;
+	list_t *new_node, *temp;
 	unsigned int len = 0;
 
-	/* Check if head is valid */
-	if (head == NULL)
+	if (str == NULL)
 		return (NULL);
 
-	/* Allocate memory for new node */
+	/* Calculate string length */
+	while (str[len])
+		len++;
+
+	/* Create new node */
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	/* Duplicate string and calculate length */
-	if (str == NULL)
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
-		new_node->str = NULL;
-		new_node->len = 0;
-	}
-	else
-	{
-		new_node->str = strdup(str);
-		if (new_node->str == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
-
-		/* Manual string length calculation */
-		while (str[len])
-			len++;
-		new_node->len = len;
+		free(new_node);
+		return (NULL);
 	}
 
-	new_node->next = NULL;  /* New node will be the last */
+	new_node->len = len;
+	new_node->next = NULL;
 
 	/* If list is empty, new node becomes head */
 	if (*head == NULL)
@@ -54,13 +43,11 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (new_node);
 	}
 
-	/* Traverse to the last node */
+	/* Find last node and add new node */
 	temp = *head;
 	while (temp->next != NULL)
 		temp = temp->next;
 
-	/* Add new node at the end */
 	temp->next = new_node;
-
 	return (new_node);
 }
