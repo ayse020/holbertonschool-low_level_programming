43a613972bf0b2145043f3ac0fb25bf118c21a82
Ayse <ayse@example.com>
2025-11-21 07:08:21 +0000

    Fix Betty style issues: replace spaces with tabs and remove leading spaces

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,23 +1,21 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_dlistint - prints all the elements of a dlistint_t list
  * @h: pointer to the head of the list
- *
  * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
-    const dlistint_t *current = h;
+	size_t count = 0;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-        count++;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
 
-    return (count);
+	return (count);
 }
