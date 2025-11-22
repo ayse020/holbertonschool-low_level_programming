36123a439059a2d49c2105308fd93c98457b4a2b
Ayse <ayse@example.com>
2025-11-22 11:21:13 +0000

    Fix: Update insert_dnodeint_at_index function and lists.h header

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -13,9 +13,9 @@
  */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+        int n;
+        struct dlistint_s *prev;
+        struct dlistint_s *next;
 } dlistint_t;
 
 /* Function prototypes */
@@ -26,5 +26,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 
 #endif /* LISTS_H */
