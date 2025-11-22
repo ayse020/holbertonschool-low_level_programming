77f8dffb27f84096ea50760599f677145014b23c
Ayse <ayse@example.com>
2025-11-22 14:39:08 +0000

    Fix: Betty style compliance - use tabs only, remove trailing whitespace

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -9,10 +9,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    /* Check if index is out of range */
-    if (index >= (sizeof(unsigned long int) * 8))
-        return (-1);
-    
-    /* Shift the bit to the rightmost position and check its value */
-    return ((n >> index) & 1);
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	return ((n >> index) & 1);
 }
