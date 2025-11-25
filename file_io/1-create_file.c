aeb98b69f34a55f401f361c7a5d9363dfb9d4370
Ayse <ayse@example.com>
2025-11-25 11:02:56 +0000

    Fix Betty coding style issues in create_file function

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,29 +12,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t bytes_written;
-    
-    /* Check if filename is NULL */
-    if (filename == NULL)
-        return (-1);
-    
-    /* Create or truncate file with rw------- permissions */
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
-    
-    /* Write text_content if it's not NULL */
-    if (text_content != NULL)
-    {
-        bytes_written = write(fd, text_content, strlen(text_content));
-        if (bytes_written == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
-    
-    close(fd);
-    return (1);
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
 }
