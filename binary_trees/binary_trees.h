a02ce15277382aeae509d31f0ea7c85b1b87bc20
Ayse <ayse@example.com>
2025-11-28 18:26:39 +0000

    Fix binary_trees.h structure and function prototypes

--- binary_trees/binary_trees.h
+++ binary_trees/binary_trees.h
@@ -13,15 +13,13 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
+} binary_tree_t;
 
 /* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
