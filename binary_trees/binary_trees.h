73365eeedba4ede634079a786df1afd11ad83f5c
Ayse <ayse@example.com>
2025-11-28 18:38:02 +0000

    Fix: use correct binary trees project structure

--- binary_trees/binary_trees.h
+++ binary_trees/binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stddef.h>
 #include <stdlib.h>
@@ -13,13 +13,15 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-} binary_tree_t;
+};
+
+typedef struct binary_tree_s binary_tree_t;
 
 /* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -27,4 +29,4 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 void binary_tree_delete(binary_tree_t *tree);
 
-#endif /* BINARY_TREES_H */
+#endif /* _BINARY_TREES_H_ */
