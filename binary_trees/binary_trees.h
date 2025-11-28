faaf009671113fe7eb608f96a20c1b5642c65161
Ayse <ayse@example.com>
2025-11-28 19:10:38 +0000

    Fix binary_trees.h syntax error - add missing #endif

--- binary_trees/binary_trees.h
+++ binary_trees/binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stddef.h>
 #include <stdlib.h>
@@ -13,15 +13,13 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
+} binary_tree_t;
 
 /* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -29,4 +27,4 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 void binary_tree_delete(binary_tree_t *tree);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /* BINARY_TREES_H */
